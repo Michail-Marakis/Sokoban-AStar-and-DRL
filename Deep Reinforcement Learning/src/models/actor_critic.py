919d98f4e25b44beb19067c1a77b981f2eccae50
Michail Marakis <p3230267@aueb.gr>
2026-07-07 14:34:11 +0300

    working on nn models

---
+++ Deep Reinforcement Learning/src/models/actor_critic.py
@@ -0,0 +1,32 @@
+class ActorCritic(nn.Module):
+
+    def __init__(self, num_actions):
+        super().__init__()
+
+        self.cnn = CNN()
+
+        self.policy = nn.Sequential(
+            nn.Linear(self.cnn.output_features,512),
+            nn.ReLU(),
+            nn.Linear(512,256),
+            nn.ReLU(),
+            nn.Linear(256,num_actions)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(self.cnn.output_features,512),
+            nn.ReLU(),
+            nn.Linear(512,256),
+            nn.ReLU(),
+            nn.Linear(256,1)
+        )
+
+
+    def forward(self,state):
+
+        features = self.cnn(state)
+
+        logits = self.policy(features)
+        value = self.value(features)
+
+        return logits,value
